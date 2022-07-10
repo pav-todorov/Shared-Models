97a177f0bd386c4eeb309a2445ee8bc7ba93cb7b
Pavel Todorov <pavel.e.todorov@gmail.com>
2022-07-10 09:21:41 +0300

    Initial Commit

---
+++ Shared Models/Shared_Models.h
@@ -0,0 +1,18 @@
+//
+//  Shared_Models.h
+//  Shared Models
+//
+//  Created by Pavel on 10.07.22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Shared_Models.
+FOUNDATION_EXPORT double Shared_ModelsVersionNumber;
+
+//! Project version string for Shared_Models.
+FOUNDATION_EXPORT const unsigned char Shared_ModelsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Shared_Models/PublicHeader.h>
+
+
